cb9d0fcc611baa7fcebe73871f778c023d046485
NourhanSalahh <nourhan.salahh1@gmail.com>
2023-04-25 19:36:58 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - calculate absolute value of input
+ * @n: input to be checked
+ * Return: absolute value of input
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
