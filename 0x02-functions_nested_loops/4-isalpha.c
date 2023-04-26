e258b26ebaca6904d9e7f646a1bff197fe312237
NourhanSalahh <nourhan.salahh1@gmail.com>
2023-04-25 19:15:13 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if the input is a letter
+ * @c: input to check
+ * Return: 1 if letter and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
